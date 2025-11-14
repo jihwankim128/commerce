a260487afeaebd6493eb01ba0187ddbdd24ab3ef
jihwankim128 <jihwan1535@nate.com>
2025-11-15 01:43:04 +0900

    refactor: payment 인수 테스트 리팩토링

--- src/test/java/learn/acceptance/PaymentAcceptanceTest.java
+++ src/test/java/learn/acceptance/PaymentAcceptanceTest.java
@@ -1,24 +1,17 @@
 package learn.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
-import java.util.Map;
 import learn.acceptance.template.BaseAcceptanceTemplate;
 import learn.acceptance.template.OrderRequestTemplate;
+import learn.acceptance.template.PaymentRequestTemplate;
 import learn.commerce.order.adapter.in.web.request.PurchaseOrderItemRequest;
 import learn.commerce.order.adapter.in.web.request.PurchaseOrderRequest;
 import learn.commerce.order.adapter.in.web.response.OrderResponse;
-import learn.commerce.order.domain.vo.OrderStatus;
+import learn.commerce.payment.adapter.in.web.dto.request.PaymentRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 class PaymentAcceptanceTest extends BaseAcceptanceTemplate {
 
@@ -26,36 +19,20 @@ class PaymentAcceptanceTest extends BaseAcceptanceTemplate {
     private OrderRequestTemplate orderRequestTemplate;
 
     @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+    private PaymentRequestTemplate template;
 
     @Test
     void 클라이언트는_주문에_대해_결제를_승인할_수_있다() throws Exception {
         // given - 주문 생성 & 결제 승인 정보
         String orderId = 주문_생성();
-        Map<String, Object> 결제요청 = Map.of(
-                "paymentKey", "test_payment_key_123",
-                "orderId", orderId,
-                "amount", 30000,
-                "method", "CARD"
-        );
+        PaymentRequest 결제요청 = new PaymentRequest("test_payment_key_123", orderId, 30000);
 
         // when
-        MvcResult 결제결과 = mockMvc.perform(post("/api/payments/confirm")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(결제요청)))
-                .andDo(print())
-                .andExpect(jsonPath("$.status").value("SUCCESS"))
-                .andExpect(jsonPath("$.body.paymentKey").value("test_payment_key_123"))
-                .andExpect(jsonPath("$.body.orderId").value(orderId))
-                .andExpect(jsonPath("$.body.amount").value(30000))
-                .andExpect(jsonPath("$.body.status").value("DONE"))
-                .andReturn();
+        template.postPaymentConfirm(결제요청);
 
         // then - 주문 상태 확인 (Policy: 주문 완료 처리)
         OrderResponse order = orderRequestTemplate.getOrder(orderId);
-        assertThat(order.status()).isEqualTo(OrderStatus.PAYMENT_FULL_FILL.name());
+        assertThat(order.status()).isEqualTo("PAYMENT_FULL_FILL");
     }
 
     private String 주문_생성() throws Exception {
---
+++ src/test/java/learn/acceptance/config/FakePaymentGateway.java
@@ -0,0 +1,28 @@
+package learn.acceptance.config;
+
+import learn.commerce.payment.application.port.dto.command.PaymentApproval;
+import learn.commerce.payment.application.port.dto.result.PaymentApprovalResult;
+import learn.commerce.payment.application.port.out.PaymentGatewayPort;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.context.annotation.Primary;
+
+@TestComponent
+@Primary
+public class FakePaymentGateway implements PaymentGatewayPort {
+    @Override
+    public PaymentApprovalResult callApproval(PaymentApproval approval) {
+        return new PaymentApprovalResult(
+                approval.paymentKey(),
+                approval.orderId(),
+                "카드",
+                approval.amount(),
+                "DONE",
+                "NOW"
+        );
+    }
+
+    @Override
+    public boolean validateApproval(String paymentStatus) {
+        return true;
+    }
+}
